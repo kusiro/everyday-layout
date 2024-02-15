ab6413ff397a073870bd47668b13561cdebe5292
MOUSSO <jackysoon871008@gmail.com>
2024-02-15 16:00:56 +0800

    feat: Add tailwindcss

--- Sesame-lab/app.vue
+++ Sesame-lab/app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1 class="text-sm">Hello test</h1>
   </div>
 </template>
---
+++ Sesame-lab/tailwind.config.ts
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,vue}',
+    './components/**/*.{js,ts,jsx,tsx,vue}',
+    './app.vue'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
