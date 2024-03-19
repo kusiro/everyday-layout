37f03c0b5efffeca496f5af4e6259a2286f5a18b
MOUSSO <jackysoon871008@gmail.com>
2024-03-19 14:23:44 +0800

    feat: Add google fonts

--- Sesame-lab/nuxt.config.ts
+++ Sesame-lab/nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    ['@nuxtjs/google-fonts', {
+      families: {
+        'Instrument Sans': true,
+      }
+    }]
+  ],
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
--- Sesame-lab/tailwind.config.ts
+++ Sesame-lab/tailwind.config.ts
@@ -6,6 +6,19 @@ export default {
     './app.vue'
   ],
   theme: {
+    screens: {
+      'sm': '480px',
+      'md': '768px',
+      'lg': '976px',
+      'xl': '1440px',
+    },
+    colors: {
+      green: '#04ddb2',
+    },
+    fontFamily: {
+      sans: ['"Instrument Sans"', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
